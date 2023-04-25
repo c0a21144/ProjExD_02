36657085c9acc7826ac35c0e59a48c1627d07bc9
azuma <c0a21144dd@edu.teu.ac.jp>
2023-04-25 17:12:27 +0900

    追加機能（未完）

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,29 @@
+import random
 import pygame as pg
 import sys
+
+delta = {
+    pg.K_UP: (0, -1),
+    pg.K_DOWN: (0, +1),
+    pg.K_LEFT: (-1, 0),
+    pg.K_RIGHT: (+1,0),
+        }
+
+def cheak_bound(scr_rct: pg.Rect, obj_rct: pg.Rect):
+    """
+    オブジェクトが画面内or画面外を判定し、
+    真理値タプルを返す関数
+    引数１ :画面surfaceのrect
+    引数２ :こうかとん、爆弾surfaceのrect
+    戻り値 :横方向、縦方向のはみ出し判定結果
+    """
+
+    yoko, tate = True, True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko  = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -7,7 +31,19 @@ def main():
     clock = pg.time.Clock()
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
+    k2_img = pg.image.load("ex02/fig/6.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
+
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    x, y = random.randint(0, 1600), random.randint(0, 900)
+    screen.blit(bb_img, [x, y])
+    vx, vy = +1, +1
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = x, y
     tmr = 0
 
     while True:
@@ -16,12 +52,30 @@ def main():
                 return 0
 
         tmr += 1
+
+        key_lst = pg.key.get_pressed()
+        for k, mv in delta.items():
+            if key_lst[k]:
+                kk_rct.move_ip(mv)
+        if cheak_bound(screen.get_rect(), kk_rct) != (True, True):
+            for k, mv in delta.items():
+                if key_lst[k]:
+                    kk_rct.move_ip(-mv[0], -mv[1])
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = cheak_bound(screen.get_rect(), bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+        screen.blit(bb_img, bb_rct)
+        if kk_rct.colliderect(bb_rct):
+            return
 
         pg.display.update()
         clock.tick(1000)
-
 
 if __name__ == "__main__":
     pg.init()
